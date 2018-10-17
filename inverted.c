97a8acfdc4faf1f29465b777fc3c7c67061d87ac
GraceNguyen1 <nguyeg9@gmail.com>
2018-10-17 21:33:17 +1100

    completed inverted.c

---
+++ inverted.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include "readData.h"
+
+/*
+ * This out put a binary tree to a file in desired format as specified
+ * in assignment requirement.
+ */
+void printBTtoFile(BTreePtr root, FILE *fptr);
+
+/*
+ * This out put a sorted list to a file in desired format as specified
+ * in assignment requirement.
+ */
+void printSLtoFile(SortedListPtr head, FILE *fptr);
+
+/*
+ * Reads data from a given collection of pages in collection.txt
+ * and generates an "inverted index" that provides a sorted list
+ * (set) of urls for every word in a given collection of pages.
+ * Before inserting words in your index, you need to "normalise"
+ * words by, removing leading and trailing spaces, converting all
+ * characters to lowercase, remove the following punctuation marks,
+ * if they appear at the end of a word: '.' (dot), ',' (comma),
+ * ';' (semicolon), ? (question mark).
+ * In each sorted list (set), duplicate urls are not allowed.
+ * This program output this "inverted index" to a file named
+ * invertedIndex.txt. One line per word, words should be
+ * alphabetically ordered, using ascending order.
+ * Each list of urls (for a single word) should be alphabetically
+ * ordered, using ascending order.
+ */
+
+int main(int argc, char *argv[]) {
+    SortedListPtr ls = NULL;
+    //read collection file to get all url data file.
+    if (argc >= 2) {
+        ls = getCollection(argv[1]);
+    } else {
+        ls = getCollection("collection.txt");
+    }
+    
+    //read each url file and build a binary tree.
+    BTreePtr t = getBTree(ls);
+    
+    FILE *fptr = fopen("invertedIndex.txt", "w");
+    
+    if (fptr == NULL) {
+        printf("Error!");
+        exit(1);
+    }
+    
+    printBTtoFile(t, fptr);
+    fclose(fptr);
+    BTfree(t);
+    return 0;
+}
+
+/*
+ * This out put a binary tree to a file in desired format as specified
+ * in assignment requirement.
+ */
+void printBTtoFile(BTreePtr root, FILE *fptr) {
+    if (root == NULL) return;
+    printBTtoFile(root->left, fptr);
+    fprintf(fptr,"%s ", root->word);
+    printSLtoFile(root->urls, fptr);
+    printBTtoFile(root->right, fptr);
+}
+
+/*
+ * This out put a sorted list to a file in desired format as specified
+ * in assignment requirement.
+ */
+void printSLtoFile(SortedListPtr head, FILE *fptr) {
+    SortedListPtr current = head;
+    while (current != NULL) {
+        fprintf(fptr,"%s ", current->val);
+        current = current->next;
+    }
+    fprintf(fptr,"%s", "\n");
+}
+
+
---
+++ inverted.h
@@ -0,0 +1,8 @@
+#ifndef INVERTED_H
+#define INVERTED_H
+
+#include "Queue.h"
+
+Queue getCollection(char* file);
+
+#endif
