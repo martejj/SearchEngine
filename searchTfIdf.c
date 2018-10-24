7aa70858e8bbfc6f6b28e766c31e8968af5dab5b
GraceNguyen1 <nguyeg9@gmail.com>
2018-10-24 13:44:24 +1100

    changed file names

---
+++ searchTfIdf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "readData.h"
+#include "inverted.h"
+
+int main(int argc, char *argv[]) {
+    
+    
+    // calculates term frequency
+    
+    // calculates inverse document frequency
+    
+    
+}
